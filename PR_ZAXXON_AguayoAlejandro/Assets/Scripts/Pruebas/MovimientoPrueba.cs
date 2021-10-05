69e0ebb45db3660d378929f7ec8e61e75571fc6e
AlejandroAguayo <alejandro77tcm@gmail.com>
2021-10-05 13:00:57 +0200

    pruebas y movimiento
    pruebas en escena de pruebas para lograr el movimiento

--- PR_ZAXXON_AguayoAlejandro/Assets/Scripts/Pruebas/MovimientoPrueba.cs
+++ PR_ZAXXON_AguayoAlejandro/Assets/Scripts/Pruebas/MovimientoPrueba.cs
@@ -18,7 +18,7 @@ public class MovimientoPrueba : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        despspeed = 10f;
     }
 
     // Update is called once per frame
@@ -28,26 +28,26 @@ public class MovimientoPrueba : MonoBehaviour
         float desplV = Input.GetAxis("Vertical");
         float posX = transform.position.x;
         float posY = transform.position.y;
-        float rot = Input.GetAxis("Horizontal-J2");
+        
 
         if (Input.GetKey(KeyCode.UpArrow))
         {
-            this.transform.Translate(Vector3.forward * Time.deltaTime);
+            this.transform.Translate(Vector3.up * Time.deltaTime * despspeed);
         }
 
         if (Input.GetKey(KeyCode.DownArrow))
         {
-            this.transform.Translate(Vector3.back * Time.deltaTime);
+            this.transform.Translate(Vector3.down * Time.deltaTime * despspeed);
         }
 
         if (Input.GetKey(KeyCode.LeftArrow))
         {
-            this.transform.Rotate(Vector3.up, -10);
+            this.transform.Translate(Vector3.left * Time.deltaTime * despspeed);
         }
 
         if (Input.GetKey(KeyCode.RightArrow))
         {
-            this.transform.Rotate(Vector3.up, 10);
+            this.transform.Translate(Vector3.right * Time.deltaTime * despspeed);
         }
         if (posX > limeteR && desplH > 0 || posX < limeteL && desplH < 0)
         {
