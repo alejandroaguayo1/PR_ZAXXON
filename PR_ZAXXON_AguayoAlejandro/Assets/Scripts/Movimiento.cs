a4b312fbd3a042f58126a3c905969b1250d92728
alejandroaguayo1 <90984444+alejandroaguayo1@users.noreply.github.com>
2021-10-12 18:14:50 +0200

    pruebas objetos
    pruebas para instanciar objetos

--- PR_ZAXXON_AguayoAlejandro/Assets/Scripts/Movimiento.cs
+++ PR_ZAXXON_AguayoAlejandro/Assets/Scripts/Movimiento.cs
@@ -35,7 +35,7 @@ public class Movimiento : MonoBehaviour
 
 
 
-        f
+        
 
     }
 
--- PR_ZAXXON_AguayoAlejandro/Assets/Scripts/Pruebas/Pruebaobst.cs
+++ PR_ZAXXON_AguayoAlejandro/Assets/Scripts/Pruebas/Pruebaobst.cs
@@ -5,23 +5,38 @@ using UnityEngine;
 public class Pruebaobst : MonoBehaviour
 {
 
-    [SerializeField] GameObject initObject;
-    InitGameScript initGameScript;
-    float speed;
+    [SerializeField] GameObject Initgame;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        initObject = GameObject.Find("InitGame");
+        StartCoroutine("Obstaculo");
 
-        initGameScript = initObject.GetComponent<InitGameScript>();
-
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-        speed = initGameScript.spaceshipSpeed;
-        transform.Translate(Vector3.back * Time.deltaTime * speed);
+
+    }
+
+
+    IEnumerator Obstaculo()
+    {
+
+
+        for (int n = 0; ; n++)
+        {
+            float aleatorioX = Random.Range(-9, 10);
+            float aleatorioY = Random.Range(-5, 6);
+            Vector3 newPos = new Vector3(aleatorioX, aleatorioY, transform.position.z);
+            Instantiate(Initgame, newPos, Quaternion.identity);
+            yield return new WaitForSeconds(0.1f);
+
+        }
+
+
+
     }
 }
